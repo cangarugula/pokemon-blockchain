70d20ce9bb79f8c2354fac7f6d85f4670658f5f3
Cang Truong <cangtruong1@gmail.com>
2018-11-06 23:49:40 -0500

    successfully adoption

--- contracts/PokemonAdoption.sol
+++ contracts/PokemonAdoption.sol
@@ -1,19 +1,20 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.21;
 
 contract PokemonAdoption {
     // an array of addresses(accounts) with length of 16
-    address[16] public trainers;
+    address[16] public adopters; // adopters
 
-    // takes an pokemon id (an integer) and returns an integer(pokemone id) after the purchases is registered as the pokemon's trainer
-    function purchase(uint pokeId) public returns (uint) {
-        require(pokeId >= 0 && pokeId <= trainers.length);
+    // takes an pokemon id (an integer) and returns an integer(pokemone id) after the adoption is registered as the pokemon's trainer
+    function adopt(uint pokeId) public returns (uint) {
+        // making sure that the pokemon's id can be found within the adopters array
+        require(pokeId >= 0 && pokeId <= 15);
 
-        trainers[pokeId] = msg.sender;
+        adopters[pokeId] = msg.sender;
         return pokeId;
     }
 
     function getAdopters() public view returns (address[16]) {
-        return trainers;
+        return adopters;
     }
 
 
--- src/js/app.js
+++ src/js/app.js
@@ -24,17 +24,34 @@ App = {
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    // we get an instance of web3 on our window from metamask
+    if (typeof web3 !== 'undefined') {
+        // if a web 3 instance is already provided by Meta MAsk.
+        // when we log into meta mask it provides us with a web 3 provider
+        App.web3Provider = web3.currentProvider;
+        // we set the one it gives us to our application's web 3 provider
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        // if it doesn't give us one, we will set a default one to our application's web 3 provider
+        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        web3 = new Web3(App.web3Provider);
+    }
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('PokemonAdoption.json', function(data) {
+        // Get the necessary contract artifact file and instantiate it with truffle-contract
+        var PokemonAdoptionArtifact = data;
+        App.contracts.PokemonAdoption = TruffleContract(PokemonAdoptionArtifact);
+
+        // Set the provider for our contract
+        App.contracts.PokemonAdoption.setProvider(App.web3Provider);
+
+        // Use our contract to retrieve and mark the adopted pokemon
+        return App.markAdopted();
+      });
 
     return App.bindEvents();
   },
@@ -44,19 +61,53 @@ App = {
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+    let adoptionInstance;
+
+    App.contracts.PokemonAdoption.deployed()
+    .then(function(instance){
+        adoptionInstance = instance
+        return adoptionInstance.getAdopters.call()
+    })
+    .then(function(adopters){
+        for(let i = 0; i < adopters.length; i++) {
+            if(adopters[i] !== "0x0000000000000000000000000000000000000000") {
+                $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true)
+            }
+        }
+    })
+    .catch(function(err) {
+        console.log(err.message)
+    })
   },
 
   handleAdopt: function(event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    let petId = parseInt($(event.target).data('id'));
+    console.log('pet id ', petId)
+    let adoptionInstance;
 
-    /*
-     * Replace me...
-     */
+    web3.eth.getAccounts(function(err, accounts) {
+        if(err) {
+            console.log(err)
+        }
+
+        let account = accounts[0]
+
+        App.contracts.PokemonAdoption.deployed()
+        .then(function(instance) {
+            adoptionInstance = instance
+            // executes the adopt function and passes it some meta data for us to store on adopters array as msg.sender
+            return adoptionInstance.adopt(petId, { from: account })
+        })
+        .then(function(result) {
+            return App.markAdopted()
+        })
+        .catch(function(err) {
+            console.log(err.message)
+        })
+    })
+
   }
 
 };
